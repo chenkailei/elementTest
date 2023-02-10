a4e4bc5990b57e26e5998c2f04f5d1a9c326e855
陈凯磊 <ckl5975@126.com>
2023-02-10 10:36:46 +0800

    add

---
+++ pages/index/index.vue
@@ -0,0 +1,21 @@
+<template>
+	<view class="container">
+		<view class="iconfont icon-31wode"></view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
